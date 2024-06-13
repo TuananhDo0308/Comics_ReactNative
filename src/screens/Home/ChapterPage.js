7c94f5712850c0abd85df1b614095ebb01b3685a
TuananhDo0308 <TuananhDo0308@gmail.com>
2024-06-13 18:19:17 +0700

    kk

--- src/screens/Home/ChapterPage.js
+++ src/screens/Home/ChapterPage.js
@@ -1,15 +1,17 @@
-// ChapterPages.js
-import React, { useEffect, useState } from 'react';
-import { View, Image, FlatList, StyleSheet, Dimensions } from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
+import { View, Image, StyleSheet, Dimensions, Animated,FlatList } from 'react-native';
 import { useRoute } from '@react-navigation/native';
 import { fetchChapterPages } from '../../api/api';
+import ChapterPagesNavigation from '../../navigations/ChapterPagesNavigation';
 
 const { width } = Dimensions.get('window');
+const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
 
 const ChapterPages = () => {
   const route = useRoute();
   const { comicId, chapterId } = route.params;
   const [pages, setPages] = useState([]);
+  const scrollY = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
     fetchChapterPages(comicId, chapterId).then((data) => {
@@ -17,15 +19,28 @@ const ChapterPages = () => {
     });
   }, [comicId, chapterId]);
 
-  return ( 
+  const navigationTranslateY = scrollY.interpolate({
+    inputRange: [0, 50],
+    outputRange: [0, 100],
+    extrapolate: 'clamp',
+  });
+
+  return (
     <View style={styles.container}>
-      <FlatList
+      <AnimatedFlatList
         data={pages}
         renderItem={({ item }) => (
           <Image source={{ uri: item }} style={styles.pageImage} resizeMode="contain" />
         )}
         keyExtractor={(item, index) => index.toString()}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+          { useNativeDriver: true }
+        )}
       />
+      <Animated.View style={[styles.navigationContainer, { transform: [{ translateY: navigationTranslateY }] }]}>
+        <ChapterPagesNavigation />
+      </Animated.View>
     </View>
   );
 };
@@ -41,5 +56,11 @@ const styles = StyleSheet.create({
     width: width,
     height: undefined,
     aspectRatio: 0.75, // Bạn có thể điều chỉnh aspectRatio theo tỷ lệ của ảnh
+  },
+  navigationContainer: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    height: 80, // Adjust according to your navigation bar height
   },
 });
